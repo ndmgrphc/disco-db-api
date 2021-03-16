043843334277f11896b611b77357df829cde9f98
Ndmgrphc <aaron@nodemographic.com>
2021-03-16 15:53:57 -0700

    query releases nested

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ fastify.get('/masters/:master_id/releases', async (req, reply) => {
 
 async function eagerLoad(connection, parentRows, table, foreignKey) {
   let ids = parentRows.map(e => e.id);
-  let [rows, fields] = await connection.query(`select * from \`${table}\` where id IN(?);`, [ids])
+  const [rows, fields] = await connection.query(`select * from \`${table}\` where id IN(?);`, [ids])
 
   let collectionKey = `${table}s`
 
@@ -143,6 +143,8 @@ async function eagerLoad(connection, parentRows, table, foreignKey) {
     
     return a;
   }, {});
+
+  return keyedByForeign;
 
   return parentRows.map(e => {
     e[collectionKey] = keyedByForeign[e.id]
