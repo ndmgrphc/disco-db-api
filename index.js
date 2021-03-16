ae9a4251581e3efe7ac7909a07215dde4c177e01
Admin <admin@Carlitos-MacBook-Pro.local>
2021-03-16 13:06:45 -0700

    query masters

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ fastify.get('/artists', async (req, reply) => {
 
   let query;
   if (req.query.search) {
-    query = ['SELECT id, name FROM artist WHERE name LIKE ?', [`%${req.query.search}%`]];
+    query = ['SELECT id, name FROM artist WHERE name LIKE ? limit 20', [`${req.query.search}%`]];
   } else if (req.query.name) {
     query = ['SELECT id, name FROM artist WHERE name = ? limit 1', [`${req.query.name}`]];
   } else {
@@ -24,7 +24,47 @@ fastify.get('/artists', async (req, reply) => {
     query[0], query[1],
   )
   connection.release()
-  return rows[0]
+  return rows
+})
+
+fastify.get('/artists/:artist_id/masters', async (req, reply) => {
+  const connection = await fastify.mysql.getConnection()
+
+  const baseSQL = `select r.master_id as id, rf.name as format, ma.artist_id, a.name as artist_name, m.title from master_artist ma 
+  inner join \`master\` m on ma.master_id = m.id
+  inner join \`release\` r on r.master_id = m.id
+  inner join release_format rf on r.id = rf.release_id
+  inner join artist a on a.id = ma.artist_id
+  WHERE`
+
+  let params = [
+    [`ma.artist_id = ?`, req.params.artist_id]
+  ];
+
+  if (req.query.search) {
+    params.push([`m.title like ?`, `${req.query.search}%`]);
+  }
+
+  if (req.query.format) {
+    params.push([`rf.name = ?`, `${req.query.format}`]);
+  }
+
+  if (req.query.country) {
+    params.push([`r.country = ?`, `${req.query.country}`]);
+  }
+
+  let sql = `${baseSQL} ${params.map(e => e[0]).join('AND')} GROUP BY r.master_id LIMIT 50;`
+
+  let query = [
+    sql,
+    params.map(e => e[1])
+  ]
+
+  const [rows, fields] = await connection.query(
+    query[0], query[1],
+  )
+  connection.release()
+  return rows
 })
 
 fastify.get('/', async (req, reply) => {
