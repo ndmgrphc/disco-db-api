f8d223e43e43c7c9d06a3525d7494eeebc895d84
Ndmgrphc <aaron@nodemographic.com>
2021-03-21 09:55:36 -0700

    master yearly report

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ fastify.get('/artists/:artist_id/masters', async (req, reply) => {
   inner join master m on m.id = r.master_id
   where ${prefetchParams.map(e => e[0]).join(' AND ')} group by r.master_id;`;
 
-  return [prefetchSql, prefetchParams.map(e => e[1]];
+  return [prefetchSql, prefetchParams.map(e => e[1])];
 
   const [prefetchRows] = await connection.query(
     prefetchSql, prefetchParams.map(e => e[1]),
