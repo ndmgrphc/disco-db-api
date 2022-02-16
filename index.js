413ab4b970c0f51eb04186b4c96c50d863011e7c
Ndmgrphc <aaron@nodemographic.com>
2022-02-16 15:39:21 -0800

    cat no

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ fastify.get('/catalog_numbers', async (req, reply) => {
     INNER JOIN release_format rf on rf.release_id = r.id
     INNER JOIN release_artist ra ON r.id = ra.release_id
     WHERE (ra.role = '' OR ra.role is null) AND rf.name = ? AND rl.normalized_catno LIKE ?
-    GROUP BY ra.artist_id, r.title, rl.normalized_catno
+    GROUP BY r.master_id, ra.artist_id, r.title, rl.normalized_catno
     ORDER BY release_count DESC
     LIMIT 25;`;
 
